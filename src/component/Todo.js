dc8e24d23a60216b4b59691fee0d4814b0ef04a1
shubham tiwari <tiwarishubham5332@gmail.com>
2021-06-04 22:44:13 +0530

    Create a React app and publish it

---
+++ src/component/Todo.js
@@ -0,0 +1,33 @@
+import React,{useState,useEffect} from 'react'
+
+function Todo()
+{
+   const [userInp,setInp]=useState('')
+   const [todo,setTodo]=useState(["Todo List"])
+   const [flag,setFlag]=useState(false)
+   function addTodo(e){
+       e.preventDefault()
+       setTodo(arr=>[...arr,userInp])
+   }
+   function changeFlag(e)
+   {
+       e.preventDefault()
+       flag?setFlag(false):setFlag(true)
+   }
+   return(
+       <>
+           <input type="text" onChange={(e)=>{setInp(e.target.value)}}></input>
+           <button onClick={(e)=>{addTodo(e)}}>Add Todo</button><br></br>
+           <button onClick={(e)=>{changeFlag(e)}}>Display Todo</button>
+           {
+               flag?todo.map((items)=>{
+                   return(
+                       <div>{items}</div>
+                   )
+               }):<div></div>
+           }
+       </>
+   )
+
+}
+export default Todo
