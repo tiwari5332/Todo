a37bfb06144d3a873cdded66b918b60304aa5ac0
shubham tiwari <tiwarishubham5332@gmail.com>
2021-06-06 00:31:39 +0530

    Interview Solution

--- src/component/Todo.js
+++ src/component/Todo.js
@@ -16,6 +16,7 @@ function Todo()
    }
    return(
        <>
+           <p>ReactJs Final Logical Test Solution</p>
            <input type="text" onChange={(e)=>{setInp(e.target.value)}}></input>
            <button onClick={(e)=>{addTodo(e)}}>Add Todo</button><br></br>
            <button onClick={(e)=>{changeFlag(e)}}>Display Todo</button>
